ed58fdb902072b0311fddef38207ede65b9397da
bahasaracoglu <bahasaracoglu@gmail.com>
2023-05-26 11:39:27 +0300

    init - migration

--- data/dbConfig.js
+++ data/dbConfig.js
@@ -1,7 +1,7 @@
 // DEĞİŞTİRMEYİN
-const knex = require('knex');
-const configurations = require('../knexfile.js');
-const environment = process.env.NODE_ENV || 'development';
+const knex = require("knex");
+const configurations = require("../knexfile.js");
+const environment = process.env.NODE_ENV || "development";
 
 // Gerçekte hangi knex yapılandırması kullanılıyor?
 // Bu, process.env.NODE_ENV'nin değerine bağlıdır!
